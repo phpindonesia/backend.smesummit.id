cbae6183921782489af7d8b725c5131b49121aad
Peter Kambey <peterjkambey@gmail.com>
2018-12-20 12:00:44 +0700

    misconfiguration as access yii2-admin

--- config/web.php
+++ config/web.php
@@ -18,23 +18,23 @@ $config = [
 			'layout' => 'left-menu',
         ]
     ],
+	'as access' => [
+		'class' => 'mdm\admin\components\AccessControl',
+		'allowActions' => [
+			'site/*',
+			//'admin/*',
+			//'some-controller/some-action',
+			// The actions listed here will be allowed to everyone including guests.
+			// So, 'admin/*' should not appear here in the production, of course.
+			// But in the earlier stages of your development, you may probably want to
+			// add a lot of actions here until you finally completed setting up rbac,
+			// otherwise you may not even take a first step.
+		]
+	],
     'components' => [
 		'authManager' => [
             'class' => 'yii\rbac\DbManager', 
         ],
-		'as access' => [
-			'class' => 'mdm\admin\components\AccessControl',
-			'allowActions' => [
-				'site/*',
-				//'admin/*',
-				//'some-controller/some-action',
-				// The actions listed here will be allowed to everyone including guests.
-				// So, 'admin/*' should not appear here in the production, of course.
-				// But in the earlier stages of your development, you may probably want to
-				// add a lot of actions here until you finally completed setting up rbac,
-				// otherwise you may not even take a first step.
-			]
-		],
 		'i18n' => [
 	        'translations' => [
 				'yii2mod.user' => [
