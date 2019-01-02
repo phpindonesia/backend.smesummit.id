290ab186d3082cc3dfd9c269acfeafdf0f8b4fae
Peter Kambey <peterjkambey@gmail.com>
2019-01-02 22:11:15 +0700

    add_telegram_features_trial_mode

---
+++ commands/TelegramController.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+use Yii;
+
+/**
+ * Telegram Notification via Cron Job.
+ *
+ * This command is use for notification via telegram on scheduled cron job.
+ *
+ * @author Peter Kambey <peter@phpindonesia.id>
+ * @since 1.0
+ */
+class TelegramController extends Controller
+{
+    public function actionSendMessage($id,$message)
+    {
+        //153273116 = chat_id peter
+        $params['chat_id'] = $id;
+        $params['text'] = $message;
+                        
+        $this->execute($params['chat_id'], $params);
+
+        echo json_encode($params);
+    }
+
+    public function actionMessageToPeter($message)
+    {
+        //153273116 = chat_id peter
+        $params['text'] = $message;
+                        
+        $this->execute(153273116, $params);
+
+        echo json_encode($params);
+    }
+
+    public function actionMessageToSc($message)
+    {
+        $params['text'] = $message;
+        $lists = [
+            'peter' => '153273116',
+            //'nur' => '558743094',
+        ];
+        
+        foreach ($lists as $list) {
+            $this->execute($list, $params);
+        }
+
+        echo json_encode($params);
+    }
+    
+    public function execute($chat_id, $params=[])
+    {
+        $client = new Client();
+        $response = $client->createRequest()
+            ->setMethod('POST')
+            ->setUrl('https://api.telegram.org/bot662538159:AAG3n4xKFgsy0irOq0GpyokQvjhOjEOX7BA/sendMessage')
+            ->setData(['chat_id'=>$chat_id,'text'=>$params['text']])
+            ->setFormat(Client::FORMAT_JSON)
+            ->send();
+            
+        return true;
+    }
+
+}
---
+++ controllers/TelegramController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Response;
+use yii\filters\AccessControl;
+use yii\httpclient\Client;
+use yii\di\Instance;
+use yii\helpers\StringHelper;
+
+class TelegramController extends \yii\rest\Controller
+{
+    public $layout=false;
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => \yii\filters\VerbFilter::className(),
+                'actions' => [
+                    //'get-webhook' => ['POST'],
+                ],
+            ],
+            'contents'=>[
+                'class' => 'yii\filters\ContentNegotiator',
+                'only' => ['*'],
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                ],
+                'languages' => [
+                    'en',
+                    'de',
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        //149.154.167.197-233 TELEGRAM  
+                        //'ips' => ['localhost','::1','149.154.167.*'],
+                        'ips' => ['*'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return [];
+    }
+
+    public function actionGetWebhook()
+    {
+        $rcv = file_get_contents('php://input');
+        $m = json_decode($rcv, true);
+
+        $text = $this->analyzeText($m);
+
+        if ($text != null) {
+            $this->sendMessage([
+                            'chat_id' => $m['message']['chat']['id'],
+                            'text' => $text
+
+                        ]);
+        } else {
+            $this->sendMessage([
+                'chat_id' => $m['message']['chat']['id'],
+                'text' => 'Mohon Maaf, Bot ini masih digunakan untuk keperluan Panitia...' 
+            ]);
+        }
+    }
+
+    public function analyzeText($m)
+    {
+        $explodes = StringHelper::explode(strtolower($m['message']['text']), ' ');
+
+        $result = null;
+
+        if ($explodes[0] == '/start') {
+            $result ='Selamat Datang! di SME Summit 2019. Bot ini masih dalam pengembangan dan baru digunakan untuk kepentingan Panitia';
+        }
+
+        if ($explodes[0] == '/id') {
+            $result =$m['message']['chat']['id'];
+        }
+            
+        return $result;
+    }
+    
+    public function sendMessage($params)
+    {
+        $client = new Client();
+        $response = $client->createRequest()
+                ->setMethod('POST')
+                ->setUrl('https://api.telegram.org/bot662538159:AAG3n4xKFgsy0irOq0GpyokQvjhOjEOX7BA/sendMessage')
+                ->setData(['chat_id'=>$params['chat_id'],'text'=>$params['text']])
+                ->setFormat(Client::FORMAT_JSON)
+                ->send();
+    }
+}
