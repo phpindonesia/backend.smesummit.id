e0baadacd4e54008583e30b13323eec3385c46ce
Nur Hidayat <hidayat365@gmail.com>
2019-02-05 23:19:57 +0700

    bugfix db connection

--- models/PaymentConfirmation.php
+++ models/PaymentConfirmation.php
@@ -34,6 +34,14 @@ class PaymentConfirmation extends \yii\db\ActiveRecord
     }
 
     /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('db2');
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function rules()
